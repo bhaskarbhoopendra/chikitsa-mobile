7a7bdb665cd098728d6d9162ad35cb07b589b7db
bhaskarbhoopendra <bhaskarbhoopendra8@gmail.com>
2023-04-10 01:53:06 +0530

    CHK-6 Firebase Env Vars

--- config.ts
+++ config.ts
@@ -2,7 +2,14 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTED_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_GRID,
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
+};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
