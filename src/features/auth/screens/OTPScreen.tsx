777abad054c1a27bbfabacbffc590931a343ed4c
bhaskarbhoopendra <bhaskarbhoopendra8@gmail.com>
2023-04-11 23:26:34 +0530

    CHK-10 OTP Screen

--- src/features/auth/screens/OTPScreen.tsx
+++ src/features/auth/screens/OTPScreen.tsx
@@ -1,9 +1,10 @@
 import { useState, FC } from "react";
-import { View, Alert, TextInput, TouchableOpacity } from "react-native";
+import { View, Alert, TouchableOpacity } from "react-native";
 import { SafeArea } from "../../../components/utility/SafeArea";
 import firebase from "firebase/compat/app";
-import { Button } from "react-native-paper";
+import { Button, TextInput, Text } from "react-native-paper";
 import { StackNavigationProps } from "./Signup";
+import styled from "styled-components/native";
 
 const OTPScreen: FC<StackNavigationProps> = ({
   route,
@@ -12,37 +13,67 @@ const OTPScreen: FC<StackNavigationProps> = ({
   const [code, setCode] = useState<string>("");
   const { verificationId } = route?.params ?? {};
 
-  const confirmCode = () => {
+  const confirmCode = async () => {
     try {
       const credential = firebase.auth.PhoneAuthProvider.credential(
         verificationId ?? "",
         code
       );
-      firebase
-        .auth()
-        .signInWithCredential(credential)
-        .then(() => {
-          setCode("");
-        })
-        .catch((err) => {
-          console.log({ err });
-        });
+      await firebase.auth().signInWithCredential(credential);
+      setCode("");
       Alert.alert("Login Successfull");
     } catch (error) {
       Alert.alert(`${error}`);
     }
   };
+
+  const Container = styled(View)`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  `;
+
+  const OTPInput = styled(TextInput)`
+    height: 70px;
+    font-size: 20px;
+    width: 15%;
+    background-color: white;
+  `;
+
+  const VerificationButton = styled(Button)`
+    background-color: ${(props) => props.theme.colors.brand.primary};
+    border-radius: 0;
+    height: 70px;
+    margin-top: 20px;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const VerificationLable = styled(Text)`
+    font-size: 18px;
+  `;
+
   return (
     <SafeArea>
       <View>
-        <TextInput
-          placeholder="Confirm Code"
-          onChangeText={setCode}
-          keyboardType="number-pad"
-        />
-
+        <Container>
+          <OTPInput
+            onChangeText={setCode}
+            mode="outlined"
+            keyboardType="number-pad"
+          />
+          <OTPInput
+            onChangeText={setCode}
+            mode="outlined"
+            keyboardType="number-pad"
+          />
+        </Container>
         <TouchableOpacity onPress={confirmCode}>
-          <Button>Confirm Verification Verification</Button>
+          <VerificationButton>
+            <VerificationLable>
+              Confirm Verification Verification
+            </VerificationLable>
+          </VerificationButton>
         </TouchableOpacity>
       </View>
     </SafeArea>
@@ -50,3 +81,13 @@ const OTPScreen: FC<StackNavigationProps> = ({
 };
 
 export default OTPScreen;
+
+// firebase
+//   .auth()
+//   .signInWithCredential(credential)
+//   .then(() => {
+//     setCode("");
+//   })
+//   .catch((err) => {
+//     console.log({ err });
+//   });
--- src/infrastrcrture/navigation/account.nagivation.tsx
+++ src/infrastrcrture/navigation/account.nagivation.tsx
@@ -25,9 +25,9 @@ const AccountNavigator = () => {
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Signup" component={Signup} />
-      <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Verify" component={OTPScreen} />
+      {/* <Stack.Screen name="Signup" component={Signup} /> */}
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 };
